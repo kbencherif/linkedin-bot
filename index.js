5c6c63ecb17e581629e96fcd16bdc8103d1bc374
Kamel <kamel.bencherif@epitech.eu>
2021-11-03 18:18:24 +0100

    :sparkles: Add login bot.

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 import * as scraper from './scraper.js'
-import { env } from './config/index.js'
 
 const browser = await scraper.runBrowser();
+const page = await scraper.openUrl(browser);
 
-const page = await scraper.scrap(browser, "http://linkedin.com");
-await page.screenshot({ path: 'example.png' });
-scraper.closeBrowser(browser);
+await scraper.loginToWebsite(page);
 
-console.log(env.url);
+scraper.closeBrowser(browser, page);
