88c5558b7b4bad91e8e565b00d4d16c3a1e1c2bd
Kamel <kbencherif29@gmail.com>
2022-02-09 16:33:09 +0100

    :building_construction: Modify infra and code architechture to make it clearer.

--- main.tf
+++ main.tf
@@ -34,20 +34,20 @@ resource "aws_iam_role" "iam_for_lambda" {
 EOF
 }
 
-data "archive_file" "zip_login" {
+data "archive_file" "zip_get_cookies" {
   type        = "zip"
-  source_dir  = "${path.module}/bot/login/"
-  excludes    = ["${path.module}/bot/login.zip"]
-  output_path = "${path.module}/bot/login/login.zip"
+  source_dir  = "${path.module}/lambdas/get_cookies/"
+  excludes    = ["${path.module}/lambdas/get_cookies.zip"]
+  output_path = "${path.module}/lambdas/get_cookies/get_cookies.zip"
 }
 
-resource "aws_lambda_function" "get_cookies_lambda" {
-  filename         = "${path.module}/bot/login/login.zip"
+resource "aws_lambda_function" "get_cookies" {
+  filename         = "${path.module}/lambdas/get_cookies/get_cookies.zip"
   role             = aws_iam_role.iam_for_lambda.arn
   function_name    = "get_cookies"
   runtime          = "nodejs14.x"
   handler          = "index.handler"
-  source_code_hash = filebase64sha256(data.archive_file.zip_login.output_path)
+  source_code_hash = filebase64sha256(data.archive_file.zip_get_cookies.output_path)
   layers           = ["arn:aws:lambda:eu-west-1:764866452798:layer:chrome-aws-lambda:25"]
   timeout          = 30
   memory_size      = 600
@@ -89,13 +89,13 @@ resource "aws_api_gateway_integration" "integration" {
 
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.get_cookies_lambda.invoke_arn
+  uri                     = aws_lambda_function.get_cookies.invoke_arn
 }
 
 resource "aws_lambda_permission" "apigw_lambda" {
   statement_id  = "AllowExecutionFromAPIGateway"
   action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.get_cookies_lambda.function_name
+  function_name = aws_lambda_function.get_cookies.function_name
   principal     = "apigateway.amazonaws.com"
 
   source_arn = "arn:aws:execute-api:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:${aws_api_gateway_rest_api.api.id}/*/${aws_api_gateway_method.lambda_get.http_method}/${aws_api_gateway_resource.api_resource.path}"
@@ -116,7 +116,7 @@ resource "aws_api_gateway_deployment" "turboflex_stage" {
 resource "aws_lambda_permission" "apigw" {
   statement_id  = "AllowAPIGatewayInvoke"
   action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.get_cookies_lambda.function_name
+  function_name = aws_lambda_function.get_cookies.function_name
   principal     = "apigateway.amazonaws.com"
 
   source_arn = "${aws_api_gateway_rest_api.api.execution_arn}/*/*"
