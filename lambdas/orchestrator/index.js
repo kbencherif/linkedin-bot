f04183eb62977856bde55d2772718aa443e31eab
Kamel <kbencherif29@gmail.com>
2022-02-28 23:55:26 +0100

    :alembic: Update orchestrator to make it use aws-sdk.

--- lambdas/orchestrator/index.js
+++ lambdas/orchestrator/index.js
@@ -1,26 +1,15 @@
 const AWS = require('aws-sdk')
 
 module.exports.handler = async () => {
-  AWS.config.update({ region: 'eu-west1' })
-  const ddb = new AWS.DynamoDB({ apiVersion: '2012-08-10' })
-  const params = {
-    TableName: 'cookies_table',
-    Key: {
-      'email': process.env.BOT_EMAIL
-    }
-  }
-
-  ddb.getItem(params, (err, data) => {
-    if (err)
-      console.log(err)
-    else
-      console.log(data)
-  })
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      status: "OK",
+  AWS.config.update({ region: 'eu-west-1' })
+  const ddb = new AWS.DynamoDB()
+  const ret = ddb.listTables(
+  ).promise()
+    .then(data => {
+      return {
+        statusCode: 200,
+        body: JSON.stringify(data)
+      }
     })
-  }
+  return await ret
 }
