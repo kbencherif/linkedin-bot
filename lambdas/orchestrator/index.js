cbea8d19a06d573177bffc33f7a9d3e5e8b5295c
Kamel <kbencherif29@gmail.com>
2022-03-01 15:06:09 +0100

    :sparkles: Implement orchestrator to check dynamodb.

--- lambdas/orchestrator/index.js
+++ lambdas/orchestrator/index.js
@@ -2,13 +2,29 @@ const AWS = require('aws-sdk')
 
 module.exports.handler = async () => {
   AWS.config.update({ region: 'eu-west-1' })
-  const ddb = new AWS.DynamoDB()
-  const ret = ddb.listTables(
-  ).promise()
+  AWS.config.logger = console
+
+  const ddb = new AWS.DynamoDB({ apiVersion: '2012-08-10' })
+  const params = {
+    TableName: "cookies_table",
+    Key: {
+      "email": { S: process.env.BOT_EMAIL }
+    }
+  }
+
+  const ret = ddb.getItem(params).promise()
     .then(data => {
       return {
         statusCode: 200,
-        body: JSON.stringify(data)
+        body: data.Item.email.S
+      }
+    })
+    .catch(err => {
+      return {
+        statusCode: err.statusCode,
+        body: JSON.stringify({
+          msg: "Error"
+        })
       }
     })
   return await ret
