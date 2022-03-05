e6d08f5db533bff0ec317ac881249c4f87da2e12
Kamel <kbencherif29@gmail.com>
2022-03-05 15:15:10 +0100

    :loud_sound: Add outputs on deployment.

---
+++ outputs.tf
@@ -0,0 +1,16 @@
+output "aws_region" {
+  value = var.aws_region
+}
+
+output "linkedin_email" {
+  value = var.bot_email
+}
+
+output "linkedin_password" {
+  value     = var.bot_password
+  sensitive = true
+}
+
+output "dynamodb_table_name" {
+  value = aws_dynamodb_table.cookies_table.name
+}
