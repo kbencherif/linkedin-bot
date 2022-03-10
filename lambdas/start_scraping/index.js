d1e0eb890991b59c1c88a8ca8a3431df58646f3a
Kamel <kbencherif29@gmail.com>
2022-03-10 13:49:38 +0100

    :recycle: Refactor lambda to improve code readability.

--- lambdas/start_scraping/index.js
+++ lambdas/start_scraping/index.js
@@ -5,51 +5,6 @@ AWS.config.update({ region: 'eu-west-1' })
 const s3 = new AWS.S3()
 const bucketName = "screenshotbucketduflex"
 
-module.exports.handler = async () => {
-  try {
-    const ddb = new AWS.DynamoDB()
-
-    await ddb.getItem({
-      TableName: process.env.COOKIES_TABLE,
-      Key: {
-        "email": { S: process.env.BOT_EMAIL }
-      }
-    }).promise().then(async data => {
-      if (!data.Item) {
-        console.error("No data")
-        return {
-          statusCode: 400,
-          body: "error"
-        }
-      } else {
-        console.log("Found data")
-        const account = AWS.DynamoDB.Converter.unmarshall(data.Item)
-        console.log(account.cookies)
-        const browser = await chrome.puppeteer.launch({
-          args: chrome.args,
-          headless: true,
-          executablePath: await chrome.executablePath
-        })
-        const page = await browser.newPage()
-        await page.goto("http://linkedin.com")
-        await page.setCookie(...account.cookies)
-        await page.reload()
-        await screenshot(page, "cookies")
-        return {
-          statusCode: 200,
-          body: "OK"
-        }
-      }
-    })
-  } catch (err) {
-    console.error(err)
-    return {
-      statusCode: 400,
-      body: "error"
-    }
-  }
-}
-
 async function screenshot(page, identifier) {
   console.log("screenshot page")
   const screenshot = await page.screenshot();
@@ -62,3 +17,51 @@ async function screenshot(page, identifier) {
   await s3.putObject(params).promise();
   console.log("put screenshot in s3")
 }
+
+const login = async (cookies) => {
+  const browser = await chrome.puppeteer.launch({
+    args: chrome.args,
+    headless: true,
+    executablePath: await chrome.executablePath
+  })
+  const page = await browser.newPage()
+  await page.goto("http://linkedin.com")
+  await page.setCookie(...cookies)
+  await page.reload()
+  return page
+}
+
+module.exports.handler = async () => {
+  try {
+    const ddb = new AWS.DynamoDB()
+    const params = {
+      TableName: process.env.COOKIES_TABLE,
+      Key: {
+        "email": { S: process.env.BOT_EMAIL }
+      }
+    }
+    const data = await ddb.getItem(params).promise()
+    if (!data.Item) {
+      console.error("No data")
+      return {
+        statusCode: 400,
+        body: "error"
+      }
+    }
+    console.log(`Found account ${process.env.BOT_EMAIL}`)
+    const account = AWS.DynamoDB.Converter.unmarshall(data.Item)
+    const page = await login(account.cookies)
+    await screenshot(page, "cookies")
+    return {
+      statusCode: 200,
+      body: "OK"
+    }
+  } catch (err) {
+    console.error(err)
+    return {
+      statusCode: 400,
+      body: "error"
+    }
+  }
+}
+
